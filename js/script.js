25594bea9c97b255ac2d1feb5713571c8bd222bd
Kate <a.katyxa@mail.ru>
2016-02-16 09:38:32 +0500

    Sum

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 (function (){	// обернем все в безымянную функцию, чтобы не создавать глобальных переменных - просто хороший тон
 	//alert("222");
-	var temp = document.createElement('div');
-		temp.className = 'addButtonSupDi';	// добавим css класс 'downloadLink' для нашей ссылки
-		temp.textContent = "+";
-			document.body.appendChild(temp); //добавляем элемент в тело документа
-			//Вот здесь вот, например.
+	var temp = document.createElement('button');
+		temp.id = 'addButtonSupDi';	// добавим css класс 'downloadLink' для нашей ссылки
+		temp.type = 'button';
+		temp.innerHTML = '+';
+		document.body.appendChild(temp); //добавляем элемент в тело документа
+
+	document.getElementById('addButtonSupDi').onclick = function() {
+    	alert("добавляем товар")
+	}
+	
+
+
 })();
