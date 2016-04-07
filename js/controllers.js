481bfa07fc97447c87f89af45cccdabb0ae52939
Midas <kingmidas1992@gmail.com>
2016-04-07 20:04:25 +0500

    Проба Ангуляр

--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,11 @@
-//var storage = chrome.storage.local;
-
-//storage.get('AllElls', function (data) {
-			//var temp = data['AllElls']; // массив массивов
-
-			var element = angular.module('phonecatApp', []);
+	var element = angular.module('phonecatApp', []);
 			element.controller('PhoneListCtrl', function ($scope)
 				{
-					 $scope.phones = [
+				var storage = chrome.storage.local;
+
+                                storage.get('AllElls', function (data) {
+			//var temp = data['AllElls']; // массив массивов	
+					 $scope.phones = data; /*[
 					    {'name': 'Nexus S',
 					     'snippet': 'Fast just got faster with Nexus S.',
 					     'price': '123' },
@@ -16,6 +15,13 @@
 					    {'name': 'MOTOROLA XOOM™',
 					     'snippet': 'The Next, Next Generation tablet.',
 					 	 'price': '333' }
-					  ];
+					  ];*/
 				});
+
+//var storage = chrome.storage.local;
+
+//storage.get('AllElls', function (data) {
+			//var temp = data['AllElls']; // массив массивов
+
+		
 //});
