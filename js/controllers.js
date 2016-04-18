47b3516553fa58fb4b2e19247197c7593710908c
Kate <a.katyxa@mail.ru>
2016-04-18 14:32:01 +0500

    12121

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 	var element = angular.module('phonecatApp', []);
 			element.controller('PhoneListCtrl', function ($scope)
 				{
-<<<<<<< HEAD
+
 				$scope.phones=[];
 				$scope.phonescount=$scope.phones.length;
 				var storage = chrome.storage.local;
@@ -13,29 +13,11 @@
 					 });
 				  });
 				});
-=======
-				var storage = chrome.storage.local;
+		var storage = chrome.storage.local;
 
                                 storage.get('AllElls', function (data) {
-			//var temp = data['AllElls']; // массив массивов	
-					 $scope.phones = data; /*[
-					    {'name': 'Nexus S',
-					     'snippet': 'Fast just got faster with Nexus S.',
-					     'price': '123' },
-					    {'name': 'Motorola XOOM™ with Wi-Fi',
-					     'snippet': 'The Next, Next Generation tablet.',
-					 	'price': '222' },
-					    {'name': 'MOTOROLA XOOM™',
-					     'snippet': 'The Next, Next Generation tablet.',
-					 	 'price': '333' }
-					  ];*/
+
+					 $scope.phones = data; 
+
 				});
 
-//var storage = chrome.storage.local;
-
-//storage.get('AllElls', function (data) {
-			//var temp = data['AllElls']; // массив массивов
-
-		
-//});
->>>>>>> origin/test
